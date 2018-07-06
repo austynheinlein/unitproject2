feb2218d1513b90dbc5b0dbc72dd3e4a9e9e5e38
your name <you@example.com>
2018-07-06 08:30:29 -0700

    updated code for heroku"

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("hi");
+const express = require('express')
+const app = express()
+const Port = proces.env.Port || 3000
+
+//******** ROUTES ********//
+app.get('/', (req, res) => {
+  res.send('this works')
+})
+
+//listener
+app.listen(Port, () => {
+  console.log("***LISTENING***");
+})
