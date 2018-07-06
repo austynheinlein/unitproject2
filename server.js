c3fd2b96f1ff0fb670fdb0a223da1232e01682b6
your name <you@example.com>
2018-07-06 08:52:53 -0700

    mongoose install

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
 
 //******** ROUTES ********//
 app.get('/', (req, res) => {
@@ -10,4 +12,9 @@ app.get('/', (req, res) => {
 //listener
 app.listen(PORT, () => {
   console.log("***LISTENING***");
+})
+//connect mongoose
+mongoose.connect(mongoUri);
+mongoose.connection.on('open', () => {
+  console.log('*********MONGOOSE CONNECT*********');
 })
