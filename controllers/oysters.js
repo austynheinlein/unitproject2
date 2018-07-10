72ddb6cb0843cd95023d0bff48a168e159ec64c4
your name <you@example.com>
2018-07-10 08:56:57 -0700

    view pages complete, added crd routes, need UPDATE route. throwing error unexpected string in my seed array, first object name:"pacific"

--- controllers/oysters.js
+++ controllers/oysters.js
@@ -4,28 +4,73 @@ const Oyster = require('../models/oysters.js')
 
 router.get('/seed', (req, res) => {
   Oyster.create([
-    {
-      name: 'Pacific',
-      location: 'Puget Sound',
-      yearsToMaturity: 2,
-      flavorProfile: ['fruity', 'sweet', 'mild brininess']
-    },
-    {
-      name: 'Olympia',
-      location: 'Totten Inlet',
-      yearsToMaturity: 4,
-      flavorProfile: ['coppery', 'high brininess']
-    },
-    {
-      name: 'Kumamoto',
-      location: 'Hammersley Inlet',
-      yearsToMaturity: 3,
-      flavorProfile: ['cucumber', 'dill', 'sweet', 'mild brininess']
-    }
-  ])
+      {
+        name:'Pacific',
+        location:'Puget Sound',
+        yearsToMaturity: 2,
+        flavorProfile:['fruity', 'sweet', 'mild brininess']
+      },
+      {
+        name:'Olympia',
+        location:'Totten Inlet',
+        yearsToMaturity: 4,
+        flavorProfile:['coppery', 'high brininess']
+      },
+      {
+        name:'Kumamoto',
+        location:'Hammersley Inlet',
+        yearsToMaturity: 3,
+        flavorProfile:['cucumber', 'dill', 'sweet', 'mild brininess']
+      }
+    ]//end array
+    (err, data) => {
+      res.redirect('/oysters')
+    }//end af
+  )//end Oyster.create
+})//end route
+
+//************************ ROUTES ***************************//
+
+// get NEW //////////////////////////////////////////////
+router.get('/new', (req, res) => {
+  res.render('new.ejs')
 })
 
-//************** ROUTES **************//
+//get INDEX //////////////////////////////////////////////
+router.get('/', (req, res) => {
+  Oyster.find({}, (err, allOysters) => {
+    res.render('index.ejs', {
+      oysters: allOysters
+    });
+  })
+})
+
+//get EDIT //////////////////////////////////////////////
+router.get('/:id/edit', (req, res) => {
+  Oyster.findById(req.params.id, (err, foundOyster) => {
+    res.render('edit.ejs',
+      {
+        oyster: foundOyster
+      }
+    )
+  })
+})
+
+//DELETE //////////////////////////////////////////////
+router.delete('/:id', (req, res) => {
+  Oyster.findByIdAndRemove(req.params.id, (err, data) => {
+    res.redirect('/oysters')
+  })
+})
+
+//get SHOW //////////////////////////////////////////////
+router.get('/:id', (req, res)=>{
+    Oyster.findById(req.params.id, (error, foundOyster)=> {
+        res.render('show.ejs', {
+            oyster:foundOyster
+        });
+    })
+})
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,26 +4,19 @@ const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
 const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
 const methodOverride = require('method-override')
+const oystersController = require('./controllers/oysters.js')
 
 
 
 //middleware
+app.use('/oysters', oystersController)
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended:false}));
 
-
-
-//******** ROUTES ********//
-// app.get('/', (req, res) => {
-//   res.send('this works')
-// })
-//get index
+// GET INDEX
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+  res.render('index.ejs', {});
 });
-
-
-
 
 //listener
 app.listen(PORT, () => {
